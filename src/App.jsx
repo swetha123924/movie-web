88fdf6e1bdac5268c06fffcdac8c77647d697410
maheswari7675 <maheswarireddi053@gmail.com>
2025-01-30 19:27:43 +0530

    movie-web

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import MovieList from './movie';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
